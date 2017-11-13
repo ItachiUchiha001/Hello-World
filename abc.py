9847286a1bca6720fbadbcd094b92c99faac3355
ItachiUchiha001 <32765926+ItachiUchiha001@users.noreply.github.com>
2017-11-13 17:38:07 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,7 @@
+P = float(input("Rs. = "))
+T = float(input("Years = "))
+R = float(input("Rate = "))
+print('Rs.',P,
+      'deposited for year' ,T,
+      'with interest rate ', R)
+print('Simple interest is', ((P*R*T)/100))
